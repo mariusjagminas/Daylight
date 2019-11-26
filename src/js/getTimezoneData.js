070b8292d5ab2e6a7460fb6ed0377171b92d3c11
mariusjagminas <mariusjagminas.it@gmail.com>
2019-11-26 20:06:41 +0100

    Move err messages to showErrorMsg component

--- src/js/getTimezoneData.js
+++ src/js/getTimezoneData.js
@@ -1,18 +1,20 @@
 import { showErrorMessage } from "./showErrorMessage";
-const NotFoundMsg = "Location not found, please try again.";
-const ServiceErrorMsg =
-  "Can't fetch data from an API. Please try it later, if the problem persists, let me know.";
 
 export const getTimezoneData = async query => {
   const encodedQuery = encodeURI(query);
-  const geodataUrl = `https://secure.geonames.org/search?q=${encodedQuery}&maxRows=1&username=${process.env.USER}&type=json`;
+  const baseUrl = "https://secure.geonames.org/";
+
   try {
-    const queryResponse = await fetch(geodataUrl);
+    const queryResponse = await fetch(
+      `${baseUrl}search?q=${encodedQuery}&maxRows=1&username=${process.env.USER}&type=json`
+    );
     const geoData = await queryResponse.json();
+
     if (geoData.totalResultsCount === 0) throw Error("NOT_FOUND");
+
     const { lat, lng, toponymName, countryCode } = geoData.geonames[0];
     const res = await fetch(
-      `https://secure.geonames.org/timezoneJSON?lat=${lat}&lng=${lng}&username=${process.env.USER}`
+      `${baseUrl}timezoneJSON?lat=${lat}&lng=${lng}&username=${process.env.USER}`
     );
     const { sunrise, sunset } = await res.json();
 
@@ -23,10 +25,6 @@ export const getTimezoneData = async query => {
       sunset
     };
   } catch (err) {
-    if (err.message == "NOT_FOUND") {
-      showErrorMessage(NotFoundMsg);
-    } else {
-      showErrorMessage(ServiceErrorMsg);
-    }
+    showErrorMessage(err.message);
   }
 };
--- src/js/showErrorMessage.js
+++ src/js/showErrorMessage.js
@@ -2,11 +2,13 @@ import { showComponent, hideComponent } from "./toggleComponent";
 import nodes from "./nodes";
 
 const { errorMessage, infoContainer, preloader } = nodes;
+const notFoundMsg = "Location not found, please try again.";
+const apiErrorMsg = "Can't fetch data from an API. Please try later";
 
 export const showErrorMessage = msg => {
   hideComponent(errorMessage);
   hideComponent(infoContainer);
   hideComponent(preloader);
-  errorMessage.innerText = msg;
+  errorMessage.innerText = msg === "NOT_FOUND" ? notFoundMsg : apiErrorMsg;
   showComponent(errorMessage);
 };
