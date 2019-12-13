f7e010767e82be2b731b302685c2a1183792a600
mariusjagminas <mariusjagminas.it@gmail.com>
2019-12-13 18:54:16 +0100

    Rename e -> element in handleFormSubmit module

--- src/js/handleFormSubmit.js
+++ src/js/handleFormSubmit.js
@@ -3,12 +3,12 @@ import { displayTimezoneData } from "./displayTimezoneData";
 import { showElement, hideElement } from "./toggleElement";
 import elements from "./elements";
 
-export const handleFormSubmit = async e => {
-  e.preventDefault();
+export const handleFormSubmit = async event => {
+  event.preventDefault();
   hideElement(elements.infoContainer);
   showElement(elements.preloader);
-  const timezoneData = await getTimezoneData(e.target[0].value);
-  e.target[0].value = "";
+  const timezoneData = await getTimezoneData(event.target[0].value);
+  event.target[0].value = "";
   displayTimezoneData(timezoneData);
   return;
 };
