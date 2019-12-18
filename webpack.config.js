6dde015d4e1ce8f9f9f9fe2de2623a090bbcb12a
mariusjagminas <mariusjagminas.it@gmail.com>
2019-12-18 16:38:36 +0100

    Add hashes to .js files

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js"
+    filename: "[name].[contenthash].js"
   },
   module: {
     rules: [
