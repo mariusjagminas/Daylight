92a1ec0c73f2b9c0010c6a9a3f0673de4ce1131c
mariusjagminas <mariusjagminas.it@gmail.com>
2020-01-02 17:07:35 +0100

    Update findNextDate function

--- __tests__/findNextDate-test.js
+++ __tests__/findNextDate-test.js
@@ -1,11 +1,15 @@
-import { findNextDate } from '../src/js/findNextDate';
+import { findNextDate } from "../src/js/findNextDate";
 
-describe('findNextDate function', () => {
-	test('should return a correct date, if the current date is after the winter solstice', () => {
-		expect(findNextDate('2019-12-27')).toBe('17th December 2020');
-	});
+describe("findNextDate function", () => {
+  test("should return a correct date, if the current date is between the winter solstice of last year and the summer solstice of the current year, and it is after 1st January.", () => {
+    expect(findNextDate("2019-01-01")).toBe("8th December 2019");
+  });
 
-	test('should return a correct date, if the current date is before the winter solstice', () => {
-		expect(findNextDate('2019-01-01')).toBe('11th December 2020');
-	});
+  test("should return a correct date, if the current date is between the winter solstice of last year and the summer solstice of the current year, and it is before 1st January.", () => {
+    expect(findNextDate("2019-12-22")).toBe("19th December 2020");
+  });
+
+  test("should return a correct date, if the current date is between the summer solstice and the winter solstice of this year", () => {
+    expect(findNextDate("2019-06-22")).toBe("20th June 2020");
+  });
 });
--- src/js/findNextDate.js
+++ src/js/findNextDate.js
@@ -1,14 +1,21 @@
-import moment from 'moment';
+import moment from "moment";
 
 export const findNextDate = date => {
-	const currentDate = moment(date);
-	const currentYear = currentDate.format('YYYY');
-	const winterSolsticeDate = moment(`${currentYear}-12-22`);
+  const currentDate = moment(date);
+  const currentYear = currentDate.format("YYYY");
+  const winterSolsticeDate = moment(`${currentYear}-12-21 00:00`);
+  const summerSolticeDate = moment(`${currentYear}-06-21 00:00`);
+  const nextSummerSolticeDate = moment(`${currentYear}-06-21 00:00`).add(
+    1,
+    "y"
+  );
+  let referencePoint = nextSummerSolticeDate;
+  if (currentDate < summerSolticeDate) referencePoint = summerSolticeDate;
+  if (currentDate > summerSolticeDate && currentDate < winterSolsticeDate)
+    referencePoint = winterSolsticeDate;
 
-	const timeInterval = winterSolsticeDate.diff(currentDate);
-	const nextDate = winterSolsticeDate.add(timeInterval);
+  const timeInterval = referencePoint.diff(currentDate);
+  const nextDate = referencePoint.add(timeInterval);
 
-	if (currentDate > winterSolsticeDate) nextDate.add(1, 'y');
-
-	return nextDate.format('Do MMMM YYYY');
+  return nextDate.format("Do MMMM YYYY");
 };
